c7a87588aff02370134b5be6618507bcb3ea0f88
Aditya K <adityak@tokemak.xyz>
2024-04-04 11:28:17 +0000

    feat: making lmpvaultrouterbase lean again

--- src/vault/LMPVaultRouterBase.sol
+++ src/vault/LMPVaultRouterBase.sol
@@ -17,232 +17,215 @@ import { IWETH9 } from "src/interfaces/utils/IWETH9.sol";
 import { LMPVault } from "src/vault/LMPVault.sol";
 
 /// @title LMPVault Router Base Contract
-abstract contract LMPVaultRouterBase is
-    ILMPVaultRouterBase,
-    SelfPermit,
-    Multicall,
-    PeripheryPayments,
-    SystemComponent
-{
-    using SafeERC20 for IERC20;
+abstract contract LMPVaultRouterBase is ILMPVaultRouterBase, SelfPermit, Multicall, PeripheryPayments, SystemComponent {
+	using SafeERC20 for IERC20;
 
-    error UserNotAllowed();
+	error UserNotAllowed();
 
-    modifier onlyAllowedUsers(ILMPVault vault, address user) {
-        if (LMPVault(address(vault))._checkUsers() && !LMPVault(address(vault)).allowedUsers(user)) {
-            revert UserNotAllowed();
-        }
-        _;
-    }
+	modifier onlyAllowedUsers(ILMPVault vault, address user) {
+		if (LMPVault(address(vault))._checkUsers() && !LMPVault(address(vault)).allowedUsers(user)) {
+			revert UserNotAllowed();
+		}
+		_;
+	}
 
-    //read weth from system registry and give it to periphery payments
-    constructor(ISystemRegistry _systemRegistry)
-        PeripheryPayments(_systemRegistry.weth())
-        SystemComponent(_systemRegistry)
-    { }
+	//read weth from system registry and give it to periphery payments
+	constructor(
+		ISystemRegistry _systemRegistry
+	) PeripheryPayments(_systemRegistry.weth()) SystemComponent(_systemRegistry) {}
 
-    //takes an amount now and do a deposit
-    // function wrapWETH9(amount ) public payable {
-    //     if (address(this).balance > 0) weth9.deposit{ value: address(this).balance }(); // wrap everything
-    // }
+	//compose a multi call here
+	/// @inheritdoc ILMPVaultRouterBase
+	function mint(
+		ILMPVault vault,
+		address to,
+		uint256 shares,
+		uint256 maxAmountIn
+	)
+		public
+		payable
+		virtual
+		override
+		onlyAllowedUsers(vault, msg.sender)
+		onlyAllowedUsers(vault, to)
+		returns (uint256 amountIn)
+	{
+		// IERC20 vaultAsset = IERC20(vault.asset());
+		// uint256 assets = vault.previewMint(shares);
 
-    //compose a multi call here
-    /// @inheritdoc ILMPVaultRouterBase
-    function mint(
-        ILMPVault vault,
-        address to,
-        uint256 shares,
-        uint256 maxAmountIn
-    )
-        public
-        payable
-        virtual
-        override
-        onlyAllowedUsers(vault, msg.sender)
-        onlyAllowedUsers(vault, to)
-        returns (uint256 amountIn)
-    {
-        IERC20 vaultAsset = IERC20(vault.asset());
-        uint256 assets = vault.previewMint(shares);
+		// if (msg.value > 0 && address(vaultAsset) == address(weth9)) {
+		// 	// We allow different amounts for different functions while performing a multicall now
+		// 	// and msg.value can be more than a single instructions amount
+		// 	// so we don't verify msg.value == assets
+		// 	_processEthIn(assets);
+		// } else {
+		// 	pullToken(vaultAsset, assets, address(this));
+		// }
+		// LibAdapter._approve(vaultAsset, address(vault), assets);
 
-        if (msg.value > 0 && address(vaultAsset) == address(weth9)) {
-            // We allow different amounts for different functions while performing a multicall now
-            // and msg.value can be more than a single instructions amount
-            // so we don't verify msg.value == assets
-            _processEthIn(assets);
-        } else {
-            pullToken(vaultAsset, assets, address(this));
-        }
-        LibAdapter._approve(vaultAsset, address(vault), assets);
+		amountIn = vault.mint(shares, to);
+		if (amountIn > maxAmountIn) {
+			revert MaxAmountError();
+		}
+	}
 
-        amountIn = vault.mint(shares, to);
-        if (amountIn > maxAmountIn) {
-            revert MaxAmountError();
-        }
-    }
+	/// @inheritdoc ILMPVaultRouterBase
+	function deposit(
+		ILMPVault vault,
+		address to,
+		uint256 amount,
+		uint256 minSharesOut
+	) public payable virtual override returns (uint256 sharesOut) {
+		// IERC20 vaultAsset = IERC20(vault.asset());
 
-    /// @inheritdoc ILMPVaultRouterBase
-    function deposit(
-        ILMPVault vault,
-        address to,
-        uint256 amount,
-        uint256 minSharesOut
-    ) public payable virtual override returns (uint256 sharesOut) {
-        IERC20 vaultAsset = IERC20(vault.asset());
+		// if (msg.value > 0 && address(vaultAsset) == address(weth9)) {
+		// 	// We allow different amounts for different functions while performing a multicall now
+		// 	// and msg.value can be more than a single instructions amount
+		// 	// so we don't verify msg.value == amount
+		// 	_processEthIn(amount);
+		// } else {
+		// 	pullToken(vaultAsset, amount, address(this));
+		// }
 
-        if (msg.value > 0 && address(vaultAsset) == address(weth9)) {
-            // We allow different amounts for different functions while performing a multicall now
-            // and msg.value can be more than a single instructions amount
-            // so we don't verify msg.value == amount
-            _processEthIn(amount);
-        } else {
-            pullToken(vaultAsset, amount, address(this));
-        }
+		if ((sharesOut = vault.deposit(amount, to)) < minSharesOut) {
+			revert MinSharesError();
+		}
+	}
 
-        return _deposit(vault, to, amount, minSharesOut);
-    }
+	// /// @dev Assumes tokens are already in the router
+	// function _deposit(
+	// 	ILMPVault vault,
+	// 	address to,
+	// 	uint256 amount,
+	// 	uint256 minSharesOut
+	// ) internal onlyAllowedUsers(vault, msg.sender) onlyAllowedUsers(vault, to) returns (uint256 sharesOut) {
+	// 	approve(IERC20(vault.asset()), address(vault), amount);
+	// 	if ((sharesOut = vault.deposit(amount, to)) < minSharesOut) {
+	// 		revert MinSharesError();
+	// 	}
+	// }
 
-    /// @dev Assumes tokens are already in the router
-    function _deposit(
-        ILMPVault vault,
-        address to,
-        uint256 amount,
-        uint256 minSharesOut
-    ) internal onlyAllowedUsers(vault, msg.sender) onlyAllowedUsers(vault, to) returns (uint256 sharesOut) {
-        approve(IERC20(vault.asset()), address(vault), amount);
-        if ((sharesOut = vault.deposit(amount, to)) < minSharesOut) {
-            revert MinSharesError();
-        }
-    }
+	/// @inheritdoc ILMPVaultRouterBase
+	function withdraw(
+		ILMPVault vault,
+		address to,
+		uint256 amount,
+		uint256 maxSharesOut
+	)
+		public
+		payable
+		virtual
+		override
+		onlyAllowedUsers(vault, msg.sender)
+		onlyAllowedUsers(vault, to)
+		returns (uint256 sharesOut)
+	{
+		sharesOut = vault.withdraw(amount, to, msg.sender);
+		if (sharesOut > maxSharesOut) {
+			revert MaxSharesError();
+		}
+	}
 
-    /// @inheritdoc ILMPVaultRouterBase
-    function withdraw(
-        ILMPVault vault,
-        address to,
-        uint256 amount,
-        uint256 maxSharesOut,
-        bool unwrapWETH
-    )
-        public
-        payable
-        virtual
-        override
-        onlyAllowedUsers(vault, msg.sender)
-        onlyAllowedUsers(vault, to)
-        returns (uint256 sharesOut)
-    {
-        address destination = unwrapWETH ? address(this) : to;
+	/// @inheritdoc ILMPVaultRouterBase
+	function redeem(
+		ILMPVault vault,
+		address to,
+		uint256 shares,
+		uint256 minAmountOut,
+		bool unwrapWETH
+	)
+		public
+		payable
+		virtual
+		override
+		onlyAllowedUsers(vault, msg.sender)
+		onlyAllowedUsers(vault, to)
+		returns (uint256 amountOut)
+	{
+		address destination = unwrapWETH ? address(this) : to;
 
-        sharesOut = vault.withdraw(amount, destination, msg.sender);
-        if (sharesOut > maxSharesOut) {
-            revert MaxSharesError();
-        }
+		if ((amountOut = vault.redeem(shares, destination, msg.sender)) < minAmountOut) {
+			revert MinAmountError();
+		}
 
-        if (unwrapWETH) {
-            _processWethOut(to);
-        }
-    }
+		if (unwrapWETH) {
+			_processWethOut(to);
+		}
+	}
 
-    /// @inheritdoc ILMPVaultRouterBase
-    function redeem(
-        ILMPVault vault,
-        address to,
-        uint256 shares,
-        uint256 minAmountOut,
-        bool unwrapWETH
-    )
-        public
-        payable
-        virtual
-        override
-        onlyAllowedUsers(vault, msg.sender)
-        onlyAllowedUsers(vault, to)
-        returns (uint256 amountOut)
-    {
-        address destination = unwrapWETH ? address(this) : to;
+	/// @inheritdoc ILMPVaultRouterBase
+	function stakeVaultToken(IERC20 vault, uint256 maxAmount) external returns (uint256) {
+		_checkVault(address(vault));
+		IMainRewarder lmpRewarder = ILMPVault(address(vault)).rewarder();
 
-        if ((amountOut = vault.redeem(shares, destination, msg.sender)) < minAmountOut) {
-            revert MinAmountError();
-        }
+		uint256 userBalance = vault.balanceOf(msg.sender);
+		if (userBalance < maxAmount) {
+			maxAmount = userBalance;
+		}
+		pullToken(vault, maxAmount, address(this));
+		approve(vault, address(lmpRewarder), maxAmount);
 
-        if (unwrapWETH) {
-            _processWethOut(to);
-        }
-    }
+		lmpRewarder.stake(msg.sender, maxAmount);
 
-    /// @inheritdoc ILMPVaultRouterBase
-    function stakeVaultToken(IERC20 vault, uint256 maxAmount) external returns (uint256) {
-        _checkVault(address(vault));
-        IMainRewarder lmpRewarder = ILMPVault(address(vault)).rewarder();
+		return maxAmount;
+	}
 
-        uint256 userBalance = vault.balanceOf(msg.sender);
-        if (userBalance < maxAmount) {
-            maxAmount = userBalance;
-        }
-        pullToken(vault, maxAmount, address(this));
-        approve(vault, address(lmpRewarder), maxAmount);
+	/// @inheritdoc ILMPVaultRouterBase
+	function withdrawVaultToken(
+		ILMPVault vault,
+		IMainRewarder rewarder,
+		uint256 maxAmount,
+		bool claim
+	) external returns (uint256) {
+		_checkVault(address(vault));
+		_checkRewarder(vault, address(rewarder));
 
-        lmpRewarder.stake(msg.sender, maxAmount);
+		uint256 userRewardBalance = rewarder.balanceOf(msg.sender);
+		if (maxAmount > userRewardBalance) {
+			maxAmount = userRewardBalance;
+		}
 
-        return maxAmount;
-    }
+		rewarder.withdraw(msg.sender, maxAmount, claim);
 
-    /// @inheritdoc ILMPVaultRouterBase
-    function withdrawVaultToken(
-        ILMPVault vault,
-        IMainRewarder rewarder,
-        uint256 maxAmount,
-        bool claim
-    ) external returns (uint256) {
-        _checkVault(address(vault));
-        _checkRewarder(vault, address(rewarder));
+		return maxAmount;
+	}
 
-        uint256 userRewardBalance = rewarder.balanceOf(msg.sender);
-        if (maxAmount > userRewardBalance) {
-            maxAmount = userRewardBalance;
-        }
+	/// @inheritdoc ILMPVaultRouterBase
+	function claimRewards(ILMPVault vault, IMainRewarder rewarder) external {
+		_checkVault(address(vault));
+		_checkRewarder(vault, address(rewarder));
 
-        rewarder.withdraw(msg.sender, maxAmount, claim);
+		// Always claims any extra rewards that exist.
+		rewarder.getReward(msg.sender, true);
+	}
 
-        return maxAmount;
-    }
+	///@dev Function assumes that vault.asset() is verified externally to be weth9
+	function _processEthIn(uint256 amount) internal {
+		if (amount > 0) {
+			// wrap eth
+			weth9.deposit{ value: amount }();
+		}
+	}
 
-    /// @inheritdoc ILMPVaultRouterBase
-    function claimRewards(ILMPVault vault, IMainRewarder rewarder) external {
-        _checkVault(address(vault));
-        _checkRewarder(vault, address(rewarder));
+	function _processWethOut(address to) internal {
+		uint256 balanceWETH9 = weth9.balanceOf(address(this));
 
-        // Always claims any extra rewards that exist.
-        rewarder.getReward(msg.sender, true);
-    }
+		if (balanceWETH9 > 0) {
+			weth9.withdraw(balanceWETH9);
+			Address.sendValue(payable(to), balanceWETH9);
+		}
+	}
 
-    ///@dev Function assumes that vault.asset() is verified externally to be weth9
-    function _processEthIn(uint256 amount) internal {
-        if (amount > 0) {
-            // wrap eth
-            weth9.deposit{ value: amount }();
-        }
-    }
+	// Helper function for repeat functionalities.
+	function _checkVault(address vault) internal view {
+		if (!systemRegistry.lmpVaultRegistry().isVault(vault)) {
+			revert Errors.ItemNotFound();
+		}
+	}
 
-    function _processWethOut(address to) internal {
-        uint256 balanceWETH9 = weth9.balanceOf(address(this));
-
-        if (balanceWETH9 > 0) {
-            weth9.withdraw(balanceWETH9);
-            Address.sendValue(payable(to), balanceWETH9);
-        }
-    }
-
-    // Helper function for repeat functionalities.
-    function _checkVault(address vault) internal view {
-        if (!systemRegistry.lmpVaultRegistry().isVault(vault)) {
-            revert Errors.ItemNotFound();
-        }
-    }
-
-    function _checkRewarder(ILMPVault vault, address rewarder) internal view {
-        if (rewarder != address(vault.rewarder()) && !vault.isPastRewarder(rewarder)) {
-            revert Errors.ItemNotFound();
-        }
-    }
+	function _checkRewarder(ILMPVault vault, address rewarder) internal view {
+		if (rewarder != address(vault.rewarder()) && !vault.isPastRewarder(rewarder)) {
+			revert Errors.ItemNotFound();
+		}
+	}
 }

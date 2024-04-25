44f1beeb56ce311a7078b61d81492488f4f094c2
Cody Nutt <23013728+codenutt@users.noreply.github.com>
2024-04-25 10:17:28 -0500

    chore: remove goerli scripts and config

--- script/utils/Constants.sol
+++ script/utils/Constants.sol
@@ -4,8 +4,6 @@ pragma solidity 0.8.17;
 
 /* solhint-disable gas-custom-errors */
 
-import { ISystemRegistry } from "src/interfaces/ISystemRegistry.sol";
-
 /// @dev Scripting specific address file.
 
 // TODO: Change placeholders when able; 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE
@@ -88,76 +86,12 @@ library Constants {
         string privateKeyEnvVar;
     }
 
-    function get(Systems system) internal view returns (Values memory) {
-        if (system == Systems.LST_GEN1_GOERLI) {
-            return getLstGen1Goerli();
-        } else if (system == Systems.LST_GEN1_MAINNET) {
+    function get(Systems system) internal pure returns (Values memory) {
+        if (system == Systems.LST_GEN1_MAINNET) {
             return getLstGen1Mainnet();
         } else {
             revert("address not found");
         }
-    }
-
-    function getLstGen1Goerli() private view returns (Values memory) {
-        ISystemRegistry registry = ISystemRegistry(0x0FE586aCF3f485BBC99e8CE05af8E2719760Ec7b);
-
-        return Values({
-            tokens: Tokens({
-                toke: 0xdcC9439Fe7B2797463507dD8669717786E51a014,
-                weth: 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6,
-                bal: 0xfA8449189744799aD2AcE7e0EBAC8BB7575eff47,
-                cvx: address(1),
-                wstEth: 0xa0494a297434eBa30e807D983605e8B12259CC21,
-                swEth: address(1),
-                stEth: address(1),
-                cbEth: address(1),
-                rEth: 0xf7bb4a608F8DFDc1a31A72bFa089c7f57545CeA9,
-                sfrxEth: 0x306C8Ca71f691f7Bb23c14B8fEA13320a35B70A6,
-                aura: address(1),
-                osEth: address(1)
-            }),
-            sys: System({
-                systemRegistry: address(registry),
-                accessController: address(registry.accessController()),
-                destinationTemplateRegistry: address(registry.destinationTemplateRegistry()),
-                destinationVaultFactory: address(registry.destinationVaultRegistry().factory()),
-                swapRouter: address(registry.swapRouter()),
-                lens: 0xbE87fb643fF79B427C42baCf5D49DC743Cc8bF3a,
-                systemSecurity: address(registry.systemSecurity()),
-                statsCalcRegistry: address(0),
-                statsCalcFactory: address(0),
-                curveResolver: address(0),
-                rootPriceOracle: address(0),
-                subOracles: SystemOracles({
-                    chainlink: address(0),
-                    ethPegged: address(0),
-                    curveV1: address(0),
-                    curveV2: address(0),
-                    balancerMeta: address(0),
-                    balancerComp: address(0),
-                    redStone: address(0),
-                    wstEth: address(0),
-                    customSet: address(0)
-                })
-            }),
-            ext: External({
-                curveMetaRegistry: address(0),
-                convexBooster: 0xF403C135812408BFbE8713b5A23a04b3D48AAE31,
-                auraBooster: address(0),
-                zeroExProxy: 0xF91bB752490473B8342a3E964E855b9f9a2A668e,
-                balancerComposableStableFactory: 0x4bdCc2fb18AEb9e2d281b0278D946445070EAda7,
-                balancerMetaStableFactor: 0xA55F73E2281c60206ba43A3590dB07B8955832Be,
-                balancerVault: 0xBA12222222228d8Ba445958a75a0704d566BF2C8,
-                mavPoolFactory: 0x6292B737E6640223EB783F1355737315985Ece49,
-                mavBoostedPositionFactory: 0x680ca064ACcEbdF5B7B8079924C5D0bb79302285
-            }),
-            pools: Pools({
-                balCompSfrxethWstethRethV1: 0x9d6d991f9dd88a93F31C1a61BccdbbC9abCF5657,
-                balMetaWethWsteth: 0x26B8Cf12405861e68230154674cE49253C3ee19b
-            }),
-            statCalcs: StatCalculators({ stEth: address(0) }),
-            privateKeyEnvVar: "GOERLI_PRIVATE_KEY"
-        });
     }
 
     function getLstGen1Mainnet() private pure returns (Values memory) {

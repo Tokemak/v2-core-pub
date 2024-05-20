c69567be6388574114363209585846aebec032a9
Aditya K <adityak@tokemak.xyz>
2024-05-20 19:09:59 +0000

    feat: add aerodrome liquidity withdrawal support

---
+++ src/destinations/adapters/AerodromeAdapter.sol
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: UNLICENSED
+// Copyright (c) 2023 Tokemak Foundation. All rights reserved.
+
+pragma solidity 0.8.17;
+
+import { IERC20 } from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+
+import { IRouter } from "src/interfaces/external/aerodrome/IRouter.sol";
+import { LibAdapter } from "src/libs/LibAdapter.sol";
+import { Errors } from "src/utils/Errors.sol";
+
+library AerodromeAdapter {
+    event WithdrawLiquidity(
+        uint256[2] amountsWithdrawn,
+        address[2] tokens,
+        // 0 - lpBurnAmount
+        // 1 - lpShare
+        // 2 - lpTotalSupply
+        uint256[3] lpAmounts,
+        address pairAddress
+    );
+
+    /**
+     * @notice A struct used to pass Aerodrome params
+     * @dev Used to avoid stack-too-deep-errors
+     * @param router Aerodrome Router contract
+     * @param tokens tokens to withdraw
+     * @param amounts min quantity of tokens to withdraw
+     * @param pool Aerodrome pool address
+     * @param stable A flag that indicates pool type
+     * @param maxLpBurnAmount max amount of LP tokens to burn for withdrawal
+     * @param deadline Execution deadline in timestamp format
+     */
+    struct AerodromeRemoveLiquidityParams {
+        address router;
+        address[] tokens;
+        uint256[] amounts;
+        address pool;
+        bool stable;
+        uint256 maxLpBurnAmount;
+        uint256 deadline;
+    }
+
+    /**
+     * @notice Withdraws liquidity from Aerodrome
+     * @dev Calls to external contract. Should be guarded with
+     * non-reentrant flags in a used contract
+     * @param params AerodromeRemoveLiquidityParams struct with all the required params
+     * @return actualAmounts Amounts of tokens received
+     */
+    function removeLiquidity(AerodromeRemoveLiquidityParams memory params) external returns (uint256[] memory) {
+        //slither-disable-start reentrancy-events
+        Errors.verifyNotZero(params.router, "router");
+        Errors.verifyNotZero(params.maxLpBurnAmount, "maxLpBurnAmount");
+        Errors.verifyNotZero(params.tokens[0], "tokens[0]");
+        Errors.verifyNotZero(params.tokens[1], "tokens[1]");
+        Errors.verifyNotZero(params.deadline, "deadline");
+        Errors.verifyNotZero(params.pool, "pool");
+
+        if (params.tokens.length != 2) revert Errors.InvalidParam("tokens.length");
+        if (params.amounts.length != 2) revert Errors.InvalidParam("amounts.length");
+        if (params.amounts[0] == 0 && params.amounts[1] == 0) revert LibAdapter.NoNonZeroAmountProvided();
+
+        LibAdapter._approve(IERC20(params.pool), address(params.router), params.maxLpBurnAmount);
+
+        // slither-disable-next-line similar-names
+        (uint256[] memory actualAmounts, uint256 lpTokenBurnAmount) = _runWithdrawal(params);
+
+        if (lpTokenBurnAmount > params.maxLpBurnAmount) {
+            revert LibAdapter.LpTokenAmountMismatch();
+        }
+        if (actualAmounts[0] < params.amounts[0]) revert LibAdapter.InvalidBalanceChange();
+        if (actualAmounts[1] < params.amounts[1]) revert LibAdapter.InvalidBalanceChange();
+
+        emit WithdrawLiquidity(
+            [actualAmounts[0], actualAmounts[1]],
+            [params.tokens[0], params.tokens[1]],
+            [lpTokenBurnAmount, IERC20(params.pool).balanceOf(address(this)), IERC20(params.pool).totalSupply()],
+            address(params.pool)
+        );
+
+        //slither-disable-end reentrancy-events
+        return actualAmounts;
+    }
+
+    ///@dev This is a helper function to avoid stack-too-deep-errors
+    function _runWithdrawal(AerodromeRemoveLiquidityParams memory _params)
+        internal
+        returns (uint256[] memory actualAmounts, uint256 lpBurnAmount)
+    {
+        uint256 lpTokensBefore = IERC20(_params.pool).balanceOf(address(this));
+        (uint256 amountA, uint256 amountB) = IRouter(_params.router).removeLiquidity(
+            _params.tokens[0],
+            _params.tokens[1],
+            _params.stable,
+            _params.maxLpBurnAmount,
+            _params.amounts[0],
+            _params.amounts[1],
+            address(this),
+            _params.deadline
+        );
+
+        uint256 lpTokensAfter = IERC20(_params.pool).balanceOf(address(this));
+
+        lpBurnAmount = lpTokensBefore - lpTokensAfter;
+
+        actualAmounts = new uint256[](2);
+        actualAmounts[0] = amountA;
+        actualAmounts[1] = amountB;
+    }
+}
---
+++ test/destinations/adapters/AerodromeAdapter.t.sol
@@ -0,0 +1,365 @@
+// SPDX-License-Identifier: UNLICENSED
+// Copyright (c) 2023 Tokemak Foundation. All rights reserved.
+pragma solidity 0.8.17;
+
+/* solhint-disable func-name-mixedcase */
+
+import { Test } from "forge-std/Test.sol";
+
+import { IERC20 } from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import { LibAdapter } from "src/libs/LibAdapter.sol";
+import { IRouter } from "src/interfaces/external/aerodrome/IRouter.sol";
+import { AerodromeAdapter } from "src/destinations/adapters/AerodromeAdapter.sol";
+import { Errors } from "src/utils/Errors.sol";
+import { AERODROME_SWAP_ROUTER_BASE, USDC_BASE, DAI_BASE, WETH9_BASE, RANDOM } from "test/utils/Addresses.sol";
+
+contract AerodromeAdapterTest is Test {
+    uint256 public baseFork;
+
+    ///@notice Auto-wrap on receive as system operates with WETH
+    receive() external payable {
+        // weth.deposit{ value: msg.value }();
+    }
+
+    function setUp() public {
+        string memory endpoint = vm.envString("BASE_MAINNET_RPC_URL");
+        baseFork = vm.createFork(endpoint, 14_406_720);
+        vm.selectFork(baseFork);
+        assertEq(vm.activeFork(), baseFork);
+    }
+
+    /**
+     * @notice Deploy liquidity to Aerodrome pool
+     * @dev Calls into external contract. Should be guarded with
+     * non-reentrant flags in a used contract
+     * @param router Balancer Vault contract
+     * @param tokenA Addresse of tokenA in the pool
+     * @param tokenB Addresse of tokenB in the pool
+     * @param stable A flag that indicates pool type
+     * @param desiredTokenAmountA Desired Amount of tokenA to deposit
+     * @param desiredTokenAmountB Desired Amount of tokenB to deposit
+     */
+    function _addLiquidity(
+        address router,
+        address tokenA,
+        address tokenB,
+        bool stable,
+        uint256 desiredTokenAmountA,
+        uint256 desiredTokenAmountB
+    ) private returns (uint256 amountA, uint256 amountB, uint256 liquidity) {
+        Errors.verifyNotZero(router, "router");
+        Errors.verifyNotZero(tokenA, "tokenA");
+        Errors.verifyNotZero(tokenB, "tokenB");
+        Errors.verifyNotZero(desiredTokenAmountA, "desiredTokenAmountA");
+        Errors.verifyNotZero(desiredTokenAmountB, "desiredTokenAmountB");
+        IERC20(tokenA).approve(router, desiredTokenAmountA);
+        IERC20(tokenB).approve(router, desiredTokenAmountB);
+        (amountA, amountB, liquidity) = IRouter(router).addLiquidity(
+            tokenA, tokenB, stable, desiredTokenAmountA, desiredTokenAmountB, 1, 1, address(this), block.timestamp
+        );
+    }
+
+    function test_validate_RevertIfZeroRouter() public {
+        address[] memory tokens = new address[](2);
+
+        tokens[0] = DAI_BASE;
+        tokens[1] = USDC_BASE;
+
+        uint256[] memory amounts = new uint256[](2);
+
+        amounts[0] = 1;
+        amounts[1] = 1;
+
+        uint256 maxLpBurnAmount = 100;
+        bool stable = true;
+        uint256 deadline = block.timestamp;
+        address pool = RANDOM;
+
+        AerodromeAdapter.AerodromeRemoveLiquidityParams memory removeLiquidityParams = AerodromeAdapter
+            .AerodromeRemoveLiquidityParams({
+            router: address(0),
+            tokens: tokens,
+            amounts: amounts,
+            pool: pool,
+            stable: stable,
+            maxLpBurnAmount: maxLpBurnAmount,
+            deadline: deadline
+        });
+
+        uint256[] memory actualAmounts = new uint256[](2);
+        vm.expectRevert(abi.encodeWithSelector(Errors.ZeroAddress.selector, "router"));
+        actualAmounts = AerodromeAdapter.removeLiquidity(removeLiquidityParams);
+    }
+
+    function test_validate_RevertIfZeroLpBurnAmount() public {
+        address[] memory tokens = new address[](2);
+
+        tokens[0] = DAI_BASE;
+        tokens[1] = USDC_BASE;
+
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0] = 1;
+        amounts[1] = 1;
+        address router = AERODROME_SWAP_ROUTER_BASE;
+
+        uint256 maxLpBurnAmount = 0;
+        bool stable = true;
+        uint256 deadline = block.timestamp;
+        address pool = RANDOM;
+
+        AerodromeAdapter.AerodromeRemoveLiquidityParams memory removeLiquidityParams = AerodromeAdapter
+            .AerodromeRemoveLiquidityParams({
+            router: router,
+            tokens: tokens,
+            amounts: amounts,
+            pool: pool,
+            stable: stable,
+            maxLpBurnAmount: maxLpBurnAmount,
+            deadline: deadline
+        });
+
+        uint256[] memory actualAmounts = new uint256[](2);
+        vm.expectRevert(abi.encodeWithSelector(Errors.InvalidParam.selector, "maxLpBurnAmount"));
+        actualAmounts = AerodromeAdapter.removeLiquidity(removeLiquidityParams);
+    }
+
+    function test_validate_RevertIfZeroToken() public {
+        address[] memory tokens = new address[](2);
+
+        tokens[0] = address(0);
+        tokens[1] = USDC_BASE;
+
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0] = 1;
+        amounts[1] = 1;
+        address router = AERODROME_SWAP_ROUTER_BASE;
+        uint256 maxLpBurnAmount = 100;
+        bool stable = true;
+        uint256 deadline = block.timestamp;
+        address pool = RANDOM;
+
+        AerodromeAdapter.AerodromeRemoveLiquidityParams memory removeLiquidityParams = AerodromeAdapter
+            .AerodromeRemoveLiquidityParams({
+            router: router,
+            tokens: tokens,
+            amounts: amounts,
+            pool: pool,
+            stable: stable,
+            maxLpBurnAmount: maxLpBurnAmount,
+            deadline: deadline
+        });
+
+        uint256[] memory actualAmounts = new uint256[](2);
+        vm.expectRevert(abi.encodeWithSelector(Errors.ZeroAddress.selector, "tokens[0]"));
+        actualAmounts = AerodromeAdapter.removeLiquidity(removeLiquidityParams);
+
+        tokens[0] = DAI_BASE;
+        tokens[1] = address(0);
+
+        removeLiquidityParams.tokens = tokens;
+
+        vm.expectRevert(abi.encodeWithSelector(Errors.ZeroAddress.selector, "tokens[1]"));
+        actualAmounts = AerodromeAdapter.removeLiquidity(removeLiquidityParams);
+    }
+
+    function test_validate_RevertIfZeroDeadline() public {
+        address[] memory tokens = new address[](2);
+
+        tokens[0] = DAI_BASE;
+        tokens[1] = USDC_BASE;
+
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0] = 1;
+        amounts[1] = 1;
+
+        address router = AERODROME_SWAP_ROUTER_BASE;
+        uint256 maxLpBurnAmount = 100;
+        bool stable = true;
+        uint256 deadline = 0;
+        address pool = RANDOM;
+
+        AerodromeAdapter.AerodromeRemoveLiquidityParams memory removeLiquidityParams = AerodromeAdapter
+            .AerodromeRemoveLiquidityParams({
+            router: router,
+            tokens: tokens,
+            amounts: amounts,
+            pool: pool,
+            stable: stable,
+            maxLpBurnAmount: maxLpBurnAmount,
+            deadline: deadline
+        });
+
+        uint256[] memory actualAmounts = new uint256[](2);
+        vm.expectRevert(abi.encodeWithSelector(Errors.InvalidParam.selector, "deadline"));
+        actualAmounts = AerodromeAdapter.removeLiquidity(removeLiquidityParams);
+    }
+
+    function test_validate_RevertIfIncorrectAmountsLength() public {
+        address[] memory tokens = new address[](2);
+
+        tokens[0] = DAI_BASE;
+        tokens[1] = USDC_BASE;
+
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = 1;
+        address router = AERODROME_SWAP_ROUTER_BASE;
+        uint256 maxLpBurnAmount = 100;
+        bool stable = true;
+        uint256 deadline = block.timestamp;
+        address pool = RANDOM;
+
+        AerodromeAdapter.AerodromeRemoveLiquidityParams memory removeLiquidityParams = AerodromeAdapter
+            .AerodromeRemoveLiquidityParams({
+            router: router,
+            tokens: tokens,
+            amounts: amounts,
+            pool: pool,
+            stable: stable,
+            maxLpBurnAmount: maxLpBurnAmount,
+            deadline: deadline
+        });
+
+        uint256[] memory actualAmounts = new uint256[](2);
+        vm.expectRevert(abi.encodeWithSelector(Errors.InvalidParam.selector, "amounts.length"));
+        actualAmounts = AerodromeAdapter.removeLiquidity(removeLiquidityParams);
+    }
+
+    function test_validate_RevertIfNoNonZeroAmount() public {
+        address[] memory tokens = new address[](2);
+
+        tokens[0] = DAI_BASE;
+        tokens[1] = USDC_BASE;
+
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0] = 0;
+        amounts[0] = 0;
+
+        address router = AERODROME_SWAP_ROUTER_BASE;
+        uint256 maxLpBurnAmount = 100;
+        bool stable = true;
+        uint256 deadline = block.timestamp;
+        address pool = RANDOM;
+
+        AerodromeAdapter.AerodromeRemoveLiquidityParams memory removeLiquidityParams = AerodromeAdapter
+            .AerodromeRemoveLiquidityParams({
+            router: router,
+            tokens: tokens,
+            amounts: amounts,
+            pool: pool,
+            stable: stable,
+            maxLpBurnAmount: maxLpBurnAmount,
+            deadline: deadline
+        });
+
+        uint256[] memory actualAmounts = new uint256[](2);
+        vm.expectRevert(abi.encodeWithSelector(LibAdapter.NoNonZeroAmountProvided.selector));
+        actualAmounts = AerodromeAdapter.removeLiquidity(removeLiquidityParams);
+    }
+
+    function test_removeLiquidityStable() public {
+        //Test with DAI , USDC
+        address tokenA = DAI_BASE;
+        address tokenB = USDC_BASE;
+        uint256 depAmountUSDC = 100 * 1e6;
+        uint256 depAmountDAI = 100 * 1e18;
+
+        deal(DAI_BASE, address(this), 2 * depAmountDAI);
+        vm.startPrank(address(0xd5c41FD4a31Eaaf5559FfCC60Ec051fcB8eCC375));
+        IERC20(USDC_BASE).transfer(address(this), 2 * depAmountUSDC);
+        vm.stopPrank();
+
+        address[] memory tokens = new address[](2);
+
+        tokens[0] = DAI_BASE;
+        tokens[1] = USDC_BASE;
+        address router = AERODROME_SWAP_ROUTER_BASE;
+        address factory = IRouter(router).defaultFactory();
+        address pool = IRouter(router).poolFor(tokenA, tokenB, true, factory);
+
+        (,, uint256 liquidity) = _addLiquidity(router, tokenA, tokenB, true, depAmountDAI, depAmountUSDC);
+
+        uint256 daiBefore = IERC20(DAI_BASE).balanceOf(address(this));
+        uint256 usdcBefore = IERC20(USDC_BASE).balanceOf(address(this));
+
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0] = 1;
+        amounts[1] = 1;
+        uint256 maxLpBurnAmount = liquidity;
+        bool stable = true;
+        uint256 deadline = block.timestamp;
+
+        AerodromeAdapter.AerodromeRemoveLiquidityParams memory removeLiquidityParams = AerodromeAdapter
+            .AerodromeRemoveLiquidityParams({
+            router: router,
+            tokens: tokens,
+            amounts: amounts,
+            pool: pool,
+            stable: stable,
+            maxLpBurnAmount: maxLpBurnAmount,
+            deadline: deadline
+        });
+
+        uint256[] memory actualAmounts = new uint256[](2);
+        actualAmounts = AerodromeAdapter.removeLiquidity(removeLiquidityParams);
+
+        uint256 daiAfter = IERC20(DAI_BASE).balanceOf(address(this));
+        uint256 usdcAfter = IERC20(USDC_BASE).balanceOf(address(this));
+
+        assertEq(daiAfter - daiBefore, actualAmounts[0]);
+        assertEq(usdcAfter - usdcBefore, actualAmounts[1]);
+    }
+
+    function test_removeLiquidityNonStable() public {
+        //Test with DAI , USDC
+        address tokenA = WETH9_BASE;
+        address tokenB = USDC_BASE;
+        uint256 depAmountUSDC = 100 * 1e6;
+        uint256 depAmountWETH9 = 100 * 1e18;
+
+        deal(WETH9_BASE, address(this), 2 * depAmountWETH9);
+        vm.startPrank(address(0xd5c41FD4a31Eaaf5559FfCC60Ec051fcB8eCC375));
+        IERC20(USDC_BASE).transfer(address(this), 2 * depAmountUSDC);
+        vm.stopPrank();
+
+        address[] memory tokens = new address[](2);
+
+        tokens[0] = WETH9_BASE;
+        tokens[1] = USDC_BASE;
+
+        address router = AERODROME_SWAP_ROUTER_BASE;
+        address factory = IRouter(router).defaultFactory();
+        address pool = IRouter(router).poolFor(tokenA, tokenB, false, factory);
+
+        (,, uint256 liquidity) = _addLiquidity(router, tokenA, tokenB, false, depAmountWETH9, depAmountUSDC);
+
+        uint256 weth9Before = IERC20(WETH9_BASE).balanceOf(address(this));
+        uint256 usdcBefore = IERC20(USDC_BASE).balanceOf(address(this));
+
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0] = 1;
+        amounts[1] = 1;
+        uint256 maxLpBurnAmount = liquidity;
+        bool stable = false;
+        uint256 deadline = block.timestamp;
+
+        AerodromeAdapter.AerodromeRemoveLiquidityParams memory removeLiquidityParams = AerodromeAdapter
+            .AerodromeRemoveLiquidityParams({
+            router: router,
+            tokens: tokens,
+            amounts: amounts,
+            pool: pool,
+            stable: stable,
+            maxLpBurnAmount: maxLpBurnAmount,
+            deadline: deadline
+        });
+
+        uint256[] memory actualAmounts = new uint256[](2);
+        actualAmounts = AerodromeAdapter.removeLiquidity(removeLiquidityParams);
+
+        uint256 weth9After = IERC20(WETH9_BASE).balanceOf(address(this));
+        uint256 usdcAfter = IERC20(USDC_BASE).balanceOf(address(this));
+
+        assertEq(weth9After - weth9Before, actualAmounts[0]);
+        assertEq(usdcAfter - usdcBefore, actualAmounts[1]);
+    }
+}

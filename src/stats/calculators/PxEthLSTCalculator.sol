dc6bcb2ff8502cebb64ca5792c9bab1f31678c17
Cody Nutt <23013728+codenutt@users.noreply.github.com>
2024-08-23 13:34:06 -0500

    feat(pxeth): add calculator

---
+++ src/stats/calculators/PxEthLSTCalculator.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+// Copyright (c) 2023 Tokemak Foundation. All rights reserved.
+pragma solidity 0.8.17;
+
+import { LSTCalculatorBase } from "src/stats/calculators/base/LSTCalculatorBase.sol";
+import { ISystemRegistry } from "src/interfaces/ISystemRegistry.sol";
+
+contract PxEthLSTCalculator is LSTCalculatorBase {
+    constructor(ISystemRegistry _systemRegistry) LSTCalculatorBase(_systemRegistry) { }
+
+    /// @inheritdoc LSTCalculatorBase
+    function calculateEthPerToken() public pure override returns (uint256) {
+        return 1 ether;
+    }
+
+    /// @inheritdoc LSTCalculatorBase
+    function isRebasing() public pure override returns (bool) {
+        return true;
+    }
+}
